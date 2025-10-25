01951e39234b1f5a8c9d60d133bff250f34c02a5
Tom Ashton <tom@ashton88.dev>
2025-10-25 16:04:00 +0100

    feat: updated updateUser DTO

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,9 +1,36 @@
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsEnum,
+  IsBoolean,
+  MinLength,
+} from 'class-validator';
 import { UserRole } from '@prisma/client';
 
 export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
   username?: string;
-  fullName?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
   role?: UserRole;
+
+  @IsBoolean()
+  @IsOptional()
   isActive?: boolean;
 }
